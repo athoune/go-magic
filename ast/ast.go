e1f072cb2c0a1e3d19bb12ff992c58694b7cb74d
Mathieu Lecarme <mathieu@garambrogne.net>
2025-08-23 20:01:52 +0200

    Magic cleaning.

--- ast/ast.go
+++ ast/ast.go
@@ -1,31 +1,29 @@
 package ast
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/athoune/go-magic/model"
 )
 
 type Runner struct {
-	Files *model.Files
+	Files model.Files
 }
 
-func NewRunner(files *model.Files) *Runner {
+func NewRunner(files model.Files) *Runner {
 	return &Runner{
 		Files: files,
 	}
 }
 
 func (r Runner) Magic(target io.ReadSeeker) (string, error) {
-	for _, file := range *r.Files {
+	for _, file := range r.Files {
 		for _, t := range file.Tests {
 			test := NewTest(t)
-			infos, err := test.Test(target)
+			_, err := test.Test(target)
 			if err != nil {
 				return "", err
 			}
-			fmt.Println(infos)
 		}
 	}
 	return "", nil
