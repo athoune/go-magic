1da09633b2b5db105443468a29e4e1ec0ace14fd
Mathieu Lecarme <mathieu@garambrogne.net>
2025-09-15 19:10:30 +0200

    fix: more doc.

--- unpack/unpack.go
+++ unpack/unpack.go
@@ -1,5 +1,12 @@
 package unpack
 
+/*
+Unpack reads and unpacks contents.
+
+Content can be read from `string` (from configuration files),
+or from `io.Reader` (from binary contents).
+*/
+
 import (
 	"encoding/binary"
 	"fmt"
